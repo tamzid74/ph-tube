21bbda9c5dd063fcabb32de059177c28921e5b95
tamzid74 <tamzidal74@gmail.com>
2023-09-01 00:16:19 +0600

    blog page create and verification icon work has done

--- script/index.js
+++ script/index.js
@@ -4,10 +4,10 @@ const handleLoadData = async() =>{
     const catagories = data.data;
     const catagoriesContainer = document.getElementById('catagories-container');
     catagories.forEach(category => {
-        // console.log(category);
+        console.log(category);
         const div = document.createElement('div');
         div.innerHTML=`
-            <button onclick="handleVideosLoader('${category.category_id}')" class="text-base text-[#252525B2] px-2 md:px-8 rounded-lg md:text-2xl focus:bg-[#FF1F3D] focus:text-white">${category?.category}</button> 
+            <button onclick="handleVideosLoader('${category.category_id}')" class="text-base text-[#252525B2] px-2 md:px-8 rounded-lg md:text-2xl focus:bg-[#FF1F3D] focus:text-white">${category?.category?category?.category:'<img src="images/Logo.png" alt="">'}</button> 
         `
         catagoriesContainer.appendChild(div);
     });
@@ -19,19 +19,33 @@ const handleLoadData = async() =>{
     const videos = data.data;
     const videosContainer = document.getElementById('videos-container');
     videosContainer.textContent = '';
-    videos.forEach(video => {
-        console.log(video);
+    videos?.forEach(video => {
+        // console.log(video);
         const div = document.createElement('div');
         div.innerHTML =`
         <div class="card bg-base-100 shadow-xl">
-        <figure class=" w-full h-52 mx-auto"><img src="${video?.thumbnail?video?.thumbnail:'no data available'}" alt="Shoes" /></figure>
-        <div class="card-body">
-          <h2 class="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div class="card-actions justify-end">
-        
-          </div>
+        <figure class=" w-full h-52 mx-auto"><img class ="w-full" src="${video?.thumbnail?video?.thumbnail:'no data available'}" alt="image loading" /></figure>
+        <div class="card-body flex flex-row items-center gap-5">
+            <div class="">
+                <img class="w-10 h-10 rounded-full" src="${video?.authors[0]?.profile_picture}" alt="image loading"/>
+                
+            </div>
+            <div>
+                <p class=" text-base font-bold">${video?.title}</p>
+                <div class="flex items-center gap-3"> 
+                <div class="mt-1">
+                <p class=" text-[#171717B2] text-base font-bold">${video?.authors[0].profile_name}</p>
+                </div>
+                <div>
+                    ${video?.authors[0]?.verified?'<img src="images/fi_10629607.png" alt="">':""}
+                </div>
+                </div>
+            </div>
         </div>
+        <p class="mx-20 py-4 text-[#171717B2] font-medium">${video?.others?.views} ${"views"}</p>
+        </div>
+        
+          
       </div>
         
         `
@@ -40,6 +54,10 @@ const handleLoadData = async() =>{
 
     });
  }
-
+handleVideosLoader("1000")
 handleLoadData()
 
+// blog page function
+function blogsPage(){
+    window.location.href ='blog.html';
+}
