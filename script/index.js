8f27986305a40a2f74a0f48e4a749a31e4830e5a
tamzid74 <tamzidal74@gmail.com>
2023-08-31 20:19:31 +0600

    categories item added form api

--- script/index.js
+++ script/index.js
@@ -1,7 +1,17 @@
 const handleLoadData = async() =>{
     const res = await fetch('https://openapi.programming-hero.com/api/videos/categories');
     const data = await res.json();
-    const categories = data.data;
-    console.log(categories);
-}
+    const videos = data.data;
+    const catagoriesContainer = document.getElementById('catagories-container');
+    videos.forEach(video => {
+        console.log(video);
+        const div = document.createElement('div');
+        div.innerHTML=`
+            <button class="text-base text-[#252525B2] px-2 md:px-8 rounded-lg md:text-2xl focus:bg-[#FF1F3D] focus:text-white">${video?.category}</button> 
+        `
+        catagoriesContainer.appendChild(div);
+    });
+    
+};
 handleLoadData()
+
