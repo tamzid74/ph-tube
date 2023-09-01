39790ef7cfa47e85ac87e4194dfa3069f2a5f9f1
tamzid74 <tamzidal74@gmail.com>
2023-09-01 17:51:31 +0600

    sort order implement done

--- script/index.js
+++ script/index.js
@@ -7,7 +7,8 @@ const handleLoadData = async() =>{
         // console.log(category);
         const div = document.createElement('div');
         div.innerHTML=`
-            <button onclick="handleVideosLoader('${category.category_id}')" class="text-base text-[#252525B2] px-2 md:px-8 rounded-lg md:text-2xl focus:bg-[#FF1F3D] focus:text-white">${category?.category?category?.category:'<img src="images/Logo.png" alt="">'}</button> 
+        <button onclick="handleVideosLoader('${category.category_id}')" class="text-base text-[#252525B2] px-2 md:px-8 rounded-lg md:text-2xl focus:bg-[#FF1F3D] hover:bg-[#FF1F3D] focus:text-white">
+        ${category?.category}</button> 
         `
         catagoriesContainer.appendChild(div);
 
@@ -54,7 +55,7 @@ const handleLoadData = async() =>{
                 <p class=" text-[#171717B2] text-base font-bold">${video?.authors[0].profile_name}</p>
                 </div>
                 <div>
-                    ${video?.authors[0]?.verified?'<img src="images/fi_10629607.png" alt="">':""}
+                    ${video?.authors[0]?.verified === true?'<img src="images/fi_10629607.png" alt="">':""}
                 </div>
                 </div>
             </div>
@@ -68,16 +69,91 @@ const handleLoadData = async() =>{
         `
         videosContainer.appendChild(div)
 
-
+        
     });
     
+    
  }
-handleVideosLoader("1000")
-handleLoadData()
+handleVideosLoader("1000");
+handleLoadData();
+const handleSort = async()=>{
+    const res = await fetch('https://openapi.programming-hero.com/api/videos/category/1000');
+    const data = await res.json();
+    const videos = data.data;
+
+    const viewCompare = (a,b)=>{
+        const viewsA = parseInt(a?.others?.views);
+        const viewsB = parseInt(b?.others?.views);
+        return viewsB - viewsA;
+    }
+    videos.sort(viewCompare)
+      const videosContainer =document.getElementById('videos-container');
+      videosContainer.textContent =" ";
+   
+    videos.forEach(video=>{
+        let sec = video?.others?.posted_date;
+        let hour = parseInt(sec/3600);
+        let min = parseInt((sec%3600)/60);
+        const viewsDataString = video?.others?.views;
+        const viewsData = parseInt(viewsDataString);
+        // console.log(viewsData);
+        const div = document.createElement('div');
+        div.innerHTML=`
+        <div class="card bg-base-100 shadow-xl">
+        <figure class=" w-full h-52 mx-auto relative">
+            <img class ="w-full" src="${video?.thumbnail?video?.thumbnail:'no data available'}" alt="image loading" />
+            <span class="absolute bg-[#171717] text-white bottom-2 right-2 p-1 rounded-lg">
+            ${hour} hours ${min} minutes ago
+            </span>
+        </figure>
+        <div class="card-body flex flex-row items-center gap-5">
+            <div>
+                <img class="w-10 h-10 rounded-full" src="${video?.authors[0]?.profile_picture}" alt="image loading"/>
+                
+            </div>
+            <div>
+                <p class=" text-base font-bold">${video?.title}</p>
+                <div class="flex items-center gap-3"> 
+                <div class="mt-1">
+                <p class=" text-[#171717B2] text-base font-bold">${video?.authors[0].profile_name}</p>
+                </div>
+                <div>
+                    ${video?.authors[0]?.verified === true?'<img src="images/fi_10629607.png" alt="">':""}
+                </div>
+                </div>
+            </div>
+        </div>
+        <p class="mx-20 py-4 text-[#171717B2] font-medium">${video?.others?.views} ${"views"}</p>
+        </div>
+        
+          
+      </div>
+        `
+        videosContainer.appendChild(div);
+    })
+
+};
+
+
+    
 
 // blog page function
 function blogsPage(){
     window.location.href ='blog.html';
 }
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
 
 
